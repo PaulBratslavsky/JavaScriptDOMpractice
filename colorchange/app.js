318583b4008a9d1d19206f6e71050e5e3e3436b3
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2019-04-24 15:50:48 -0400

    ...

--- colorchange/app.js
+++ colorchange/app.js
@@ -7,12 +7,19 @@ const arrayLength = colors.length;
 // target elements
 const mainDiv = document.getElementById('root');
 const button = document.getElementById('button');
+const showColor = document.getElementById('showColor');
 
 // generate random number based on array length
 const getRandomNumber = (arrayLength) => Math.floor( (Math.random() * arrayLength) );
 
 // change colors vis style attribute
-const changeColor = () => { mainDiv.style.backgroundColor = colors[getRandomNumber(arrayLength)]; }
+const changeColor = () => { 
+  const arrayIndex = colors[getRandomNumber(arrayLength)];
+  showColor.innerHTML = `<h1>${arrayIndex}</h1>`;
+  mainDiv.style.backgroundColor = arrayIndex; 
+  console.log(showColor);
+  
+}
 
 // event listener
 button.addEventListener('click', changeColor);
